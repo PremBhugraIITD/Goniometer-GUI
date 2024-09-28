2bda586cdab84725f3d2f585796fd12c2c2064ba
Prem-Bhugra <ch7221038@iitd.ac.in>
2024-09-28 15:25:58 +0530

    Controller Section half done

--- client/src/components/ControllerSection/ControllerSection.jsx
+++ client/src/components/ControllerSection/ControllerSection.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import {Link} from "react-router-dom";
 import "./ControllerSection.css";
 
 const ControllerSection = () => {
     return <div className="controller-container">
-        
+        <div className="button-area">
+            <h2>Controller Choice</h2>
+            <Link>Button 1</Link>
+            <Link>Button 2</Link>
+            <Link>Button 3</Link>
+        </div>
+        <div className="input-area"></div>
     </div>;
 };
 
